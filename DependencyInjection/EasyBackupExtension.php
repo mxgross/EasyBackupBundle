c78a672addce8e953f486f7ee104a4dc533ab995
mxgross <info@maximiliangross.de>
2019-12-22 13:40:08 +0100

    Tried to fix use declaration problems

--- DependencyInjection/EasyBackupExtension.php
+++ DependencyInjection/EasyBackupExtension.php
@@ -1,29 +1,37 @@
 <?php
 
+/*
+ * This file is part of the Kimai DemoBundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace KimaiPlugin\EasyBackupBundle\DependencyInjection;
 
+use App\Plugin\AbstractPluginExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use App\Plugin\AbstractPluginExtension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class EasyBackupExtension extends AbstractPluginExtension implements PrependExtensionInterface
 {
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader(
-            $container, 
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
-
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         $this->registerBundleConfiguration($container, $config);
         $container->setParameter('easy_settings', $config);
 
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
 
