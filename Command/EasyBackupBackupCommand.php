37df42efa3eb865683b1a74b651070b679704d60
mgross <maximiliano_gross@yahoo.de>
2023-01-03 00:07:19 +0100

    Fixed permission problems

--- Command/EasyBackupBackupCommand.php
+++ Command/EasyBackupBackupCommand.php
@@ -3,22 +3,16 @@
 namespace KimaiPlugin\EasyBackupBundle\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\HttpClient\HttpClient;
-
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 // the name of the command is what users type after "php bin/console"
 #[AsCommand(name: 'EasyBackup:backup',
             description: 'Creates a new backup.',
             hidden: false,
             )]
-class EasyBackupBackupCommand extends Command
+class EasyBackupBackupCommand extends ContainerAwareCommand
 {
     protected static $defaultName = 'EasyBackup:backup';
     // the command description shown when running "php bin/console list"
@@ -26,10 +20,9 @@ class EasyBackupBackupCommand extends Command
 
     private $router;
 
-    public function __construct(RouterInterface $router, TokenStorageInterface $tokenStorage)
+    public function __construct(RouterInterface $router)
     {
         $this->router = $router;
-        $this->tokenStorage = $tokenStorage;
 
         parent::__construct();
     }
@@ -44,15 +37,13 @@ class EasyBackupBackupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $url = $this->router->generate('create_backup');
+        $container = $this->getContainer();
+        $controller = $container->get('KimaiPlugin\EasyBackupBundle\Controller\EasyBackupController');
+        $result = $controller->createBackupAction();
 
-        $token = new UsernamePasswordToken('susan_super', 'kitten', 'main', ['ROLE_ADMIN']); // TODO: FIX ME
-        //$this->tokenStorage->setToken($token);
+        $output->writeln($result);
+        //$output->writeln($response->getContent());
 
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://localhost/kimai2/public/index.php' . $url); // TODO: FIX ME
-
-        $output->writeln($response->getContent());
         // this method must return an integer number with the "exit status code"
         // of the command. You can also use these constants to make code more readable
 
